72380e369683b65eb7e6ec8be625b9e3344fad16
Aliology <ae90927@gmail.com>
2025-09-18 15:59:34 +0300

    Add files via upload

---
+++ MVC/MVC D01 G01(Ali Emad Ali)/Practice/Practice/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class ProductController : Controller
+    {
+        // http://localhost:21127/Product/Laptops
+        public IActionResult Laptops()
+        {
+            return View("Laptops");
+        }
+
+        // http://localhost:21127/Product/Medicine
+        public IActionResult Medicine()
+        {
+            return View("Medicine");
+        }
+    }
+}
