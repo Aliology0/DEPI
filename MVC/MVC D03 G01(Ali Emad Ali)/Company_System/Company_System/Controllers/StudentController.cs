ee57eaa4a66bb6b2551d1898be35f55330140645
Ali Emad <ae90927@gmail.com>
2025-09-24 11:17:56 +0300

    Adding Day 3 Task

---
+++ MVC/MVC D03 G01(Ali Emad Ali)/Company_System/Company_System/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Company_System.Data;
+using Company_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company_System.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly CompanyDbContext _context;
+
+        public StudentController(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        // /Student/ShowAll
+        public IActionResult ShowAll()
+        {
+            var studentBL = new StudentBL(_context);
+            List<Student> studentList = studentBL.GetAll();
+            return View(studentList);
+        }
+
+        // /Student/ShowDetails
+        public IActionResult ShowDetails(int id)
+        {
+            StudentBL studentBL = new StudentBL(_context);
+            Student student = studentBL.GetById(id);
+
+            if (student == null)
+                return NotFound();
+
+
+            return View("GetById", student);
+        }
+    }
+}
---
+++ MVC/MVC D03 G01(Ali Emad Ali)/Company_System/Company_System/Data/CompanyDbContext.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Company_System.Models;
+
+namespace Company_System.Data
+{
+    public class CompanyDbContext : DbContext
+    {
+        #region DbSets
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StudentCourses> StudentCourses { get; set; }
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=CompanyDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region DepartmentConfiguration
+            modelBuilder.Entity<Department>()
+                .HasKey(d => d.Id);
+
+            modelBuilder.Entity<Department>()
+                .HasMany(d => d.Students)
+                .WithOne(s => s.Department)
+                .HasForeignKey(s => s.DepartmentId);
+
+            modelBuilder.Entity<Department>()
+                .HasMany(d => d.Courses)
+                .WithOne(c => c.Department)
+                .HasForeignKey(c => c.DepartmentId);
+
+            modelBuilder.Entity<Department>()
+                .HasMany(d => d.Teachers)
+                .WithOne(t => t.Department)
+                .HasForeignKey(t => t.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+            #endregion
+
+            #region StudentConfiguration
+            modelBuilder.Entity<Student>()
+                .HasKey(s => s.Id);
+            #endregion
+
+            #region CourseConfiguration
+            modelBuilder.Entity<Course>()
+                .HasKey(c => c.Id);
+            #endregion
+
+            #region TeacherConfiguration
+            modelBuilder.Entity<Teacher>()
+                .HasKey(t => t.Id);
+
+            modelBuilder.Entity<Teacher>()
+                .HasOne(t => t.Course)
+                .WithMany(c => c.Teachers)
+                .HasForeignKey(t => t.CourseId);
+
+            modelBuilder.Entity<Teacher>()
+                .Property(t => t.Salary)
+                .HasColumnType("money");
+            #endregion
+
+            #region StudentCoursesConfiguration
+            modelBuilder.Entity<StudentCourses>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourses>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<StudentCourses>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+            #endregion
+        }
+
+    }
+}
