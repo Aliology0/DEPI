92271db57887039baa9840ac9cd38cd017585159
Ali Emad <ae90927@gmail.com>
2025-11-06 18:49:43 +0200

    Adding Day 1 Task

---
+++ API/API D01 G01(Ali Emad Ali)/Day01/Day01/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day01.Models
+{
+    public class Course
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        public string? Crs_name { get; set; }
+
+        [MaxLength(150)]
+        public string? crs_desc { get; set; }
+
+        public int? Duration { get; set; }
+    }
+}
---
+++ API/API D01 G01(Ali Emad Ali)/Day01/Day01/Models/CourseContext.cs
@@ -0,0 +1,12 @@
+﻿using Day01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day01.Models
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(DbContextOptions<CourseContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
